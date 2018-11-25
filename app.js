51234a1f1693371659f82b345d7f0826e3f4c4d5
Akhil Seshan <akhilses@gmail.com>
2018-11-25 14:16:16 +0530

    modification in app.js

--- app.js
+++ app.js
@@ -21,11 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/book',indexRouter);
-
-app.get('/book', (req, res) => {
-  res.render("book.ejs");
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
