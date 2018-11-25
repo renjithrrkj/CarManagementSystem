47db4c146d6f7f950e2168a60de133da2bbf61dc
Akhil Seshan <akhilses@gmail.com>
2018-11-25 14:12:20 +0530

    added new files

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Car Service Management System' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Car Service Management System'
+  });
 });
 
+router.get('/book', function(req,res,next){
+  res.render('book',{
+    title: 'Book'
+  });
+});
 module.exports = router;
